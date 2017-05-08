2138a6a0fcae1df49ab6a03fc044582aefcf09d6
Stilyan-Kangalov <stilyan_kangalov@abv.bg>
2017-05-08 08:15:40 +0300

    Automated commit by Speedy_Git on 08/05/2017 08:15

--- ShadowGit/ShadowGIt.rb
+++ ShadowGit/ShadowGIt.rb
@@ -9,18 +9,19 @@ puts
 
 $dir = Dir.pwd
 approve = ""
-time = Time.new
 
 FileWatcher.new([$dir]).watch() do |filename, event|
-  puts "Do you approve the new change at " + time.hour + ":" + time.min + ":" + time.sec + " to be commited?"
+  puts "Do you approve the new change at to be commited?"
   approve = gets.chomp
   if(event == :changed and approve == "y")
+    puts "=-=-="
     puts "File updated: " + filename
 	puts "=-=-="
 	puts
 	system "start " + $dir + "/Speedy_Git.bat"
   end
   if(event == :delete and approve == "y")
+    puts "=-=-="
     puts "New File Action: " + filename
 	puts "=-=-="
 	puts
