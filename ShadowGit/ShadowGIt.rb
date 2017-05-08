d557b89097db4f26a8dac46588f3e0f8f5f597c9
Stilyan-Kangalov <stilyan_kangalov@abv.bg>
2017-05-08 08:13:36 +0300

    Automated commit by Speedy_Git on 08/05/2017 08:13

--- ShadowGit/ShadowGIt.rb
+++ ShadowGit/ShadowGIt.rb
@@ -1,15 +1,29 @@
 require 'filewatcher'
+puts "###############################################################################"
+puts "############################## Shadow Git #####################################"
+puts
+puts "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+puts "Start Monitoring: " + Dir.pwd
+puts "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+puts
 
-puts "Lets Start Monitoring"
+$dir = Dir.pwd
+approve = ""
+time = Time.new
 
-FileWatcher.new(["G:/Side_Project/Snipenator"]).watch() do |filename, event|
-  if(event == :changed)
+FileWatcher.new([$dir]).watch() do |filename, event|
+  puts "Do you approve the new change at " + time.hour + ":" + time.min + ":" + time.sec + " to be commited?"
+  approve = gets.chomp
+  if(event == :changed and approve == "y")
     puts "File updated: " + filename
-	system "start G:\\Side_Project\\Snipenator\\Speedy_Git.bat"
+	puts "=-=-="
+	puts
+	system "start " + $dir + "/Speedy_Git.bat"
   end
-  if(event == :delete)
+  if(event == :delete and approve == "y")
     puts "New File Action: " + filename
-	system "start G:\\Side_Project\\Snipenator\\Speedy_Git.bat"
+	puts "=-=-="
+	puts
+	system "start " + $dir + "/Speedy_Git.bat"
   end
 end
-
