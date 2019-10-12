71636c158e78ac2160a3b6b714e958e31990dae4
Stilyan <Stilyan-Kangalov@users.noreply.github.com>
2019-10-12 20:06:44 +0300

    Update Speedy_Git.bat

--- SpeedyGit/Speedy_Git.bat
+++ SpeedyGit/Speedy_Git.bat
@@ -19,8 +19,11 @@ echo   Available commands are:
 echo    1. Push with automated commit.
 echo    2. Push with custom message.
 echo    3. Pull new updates.
-echo    4. Check your notifications at Github.
-echo    5. Exit.
+echo    4. Pull and Merge updates from a forked repo.
+echo    5. Check your notifications at Github.
+echo    6. Show me all remotes...
+echo    7. Add an upstream remote from a forked repo.
+echo    8. Exit.
 set /p "option=### Make your choice:"
 echo.
 echo =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
@@ -50,12 +53,34 @@ IF "%option%"=="1" (
 	echo ### Great, everything went well! Press any key for exit.
 	pause >nul
 ) ELSE IF "%option%"=="4" (
+	echo ### Processing your request.
+    call git fetch upstream
+    echo ### All updates are in your upstream branch. Now, press any key to trigger the merge!
+    timeout /t -1
+    call git merge upstream/master
+	echo.
+	echo ### Great, everything went well! Press any key for exit.
+	pause >nul
+) ELSE IF "%option%"=="5" (
 	start https://github.com/notifications
 	echo ### Processing your request.
 	echo.
 	echo ### Great, everything went well! Press any key for exit.
 	pause >nul
-) ELSE IF "%option%"=="5" (
+) ELSE IF "%option%"=="6" (
+	call git remote -v
+    echo =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+	echo.
+	echo ### Great, everything went well! Press any key for exit.
+    pause >nul
+) ELSE IF "%option%"=="7" (
+	set /p "url=### Paste the URL from the original(forked) repo and press Enter:"
+    call git remote add upstream %url%
+    echo =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+	echo.
+	echo ### Great, everything went well! Press any key for exit.
+    pause >nul
+) ELSE IF "%option%"=="8" (
 	exit
 ) ELSE (
     exit
