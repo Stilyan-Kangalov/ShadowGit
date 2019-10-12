b5b648a1ab91b613a3a5b40cb1e3e92b7bd0c684
Stilyan Kangalov <stilyan_kangalov@abv.bg>
2019-10-12 20:22:46 +0300

    Automated commit by Speedy_Git on Sat 10/12/2019 08:22 PM

--- SpeedyGit/Speedy_Git.bat
+++ SpeedyGit/Speedy_Git.bat
@@ -10,19 +10,16 @@ for /f "delims=" %%a in ('time/t') do @set mytime=%%a
 set currentTime=%mydate%%mytime%
 call git status
 echo =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-
-echo.
-set /p "msgline=### Type message for your new commit:"
 echo.
 echo ### Choose to Push or Get ###
 echo   Available commands are:
 echo    1. Push with automated commit.
 echo    2. Push with custom message.
 echo    3. Pull new updates.
-echo    4. Pull and Merge updates from a forked repo.
-echo    5. Check your notifications at Github.
-echo    6. Show me all remotes...
-echo    7. Add an upstream remote from a forked repo.
+echo    4. Sync updates from a forked repo. (Pull, Merge and Push)
+echo    5. Show me all remotes...
+echo    6. Add an upstream remote from a forked repo.
+echo    7. Check your notifications at Github.
 echo    8. Exit.
 set /p "option=### Make your choice:"
 echo.
@@ -39,6 +36,7 @@ IF "%option%"=="1" (
 	pause >nul
 ) ELSE IF "%option%"=="2" (
 	REM make new commit with your custom message
+	set /p "msgline=### Type message for your new commit:"
 	call git add .
 	call git commit -m "%msgline%"
 	call git push origin master
@@ -58,28 +56,30 @@ IF "%option%"=="1" (
     echo ### All updates are in your upstream branch. Now, press any key to trigger the merge!
     timeout /t -1
     call git merge upstream/master
+	timeout /t 5
+	call git push origin master
 	echo.
 	echo ### Great, everything went well! Press any key for exit.
 	pause >nul
 ) ELSE IF "%option%"=="5" (
-	start https://github.com/notifications
-	echo ### Processing your request.
-	echo.
-	echo ### Great, everything went well! Press any key for exit.
-	pause >nul
-) ELSE IF "%option%"=="6" (
 	call git remote -v
     echo =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 	echo.
 	echo ### Great, everything went well! Press any key for exit.
     pause >nul
-) ELSE IF "%option%"=="7" (
+) ELSE IF "%option%"=="6" (
 	set /p "url=### Paste the URL from the original(forked) repo and press Enter:"
     call git remote add upstream %url%
     echo =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 	echo.
 	echo ### Great, everything went well! Press any key for exit.
     pause >nul
+) ELSE IF "%option%"=="7" (
+	start https://github.com/notifications
+	echo ### Processing your request.
+	echo.
+	echo ### Great, everything went well! Press any key for exit.
+	pause >nul
 ) ELSE IF "%option%"=="8" (
 	exit
 ) ELSE (
